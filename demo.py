c64d533f58b09a7a34fba3c4a655a78b8fcb334d
Aryamaan Pandey <aryamaanpandey@L-TMVR6D67G9.local>
2023-01-20 10:14:04 +0530

    Python concepts

---
+++ demo.py
@@ -0,0 +1,14 @@
+import json
+
+# a Python object (dict):
+def convert(dict):
+
+# convert into JSON:
+    y = json.dumps(dict)
+    return y
+
+"""
+def ab():
+    print('Hello')
+
+"""
---
+++ import_package.py
@@ -0,0 +1,27 @@
+
+# Import classes from your brand new package
+from cars import Bmw
+from cars import Audi
+from cars import Nissan
+   
+# Create an object of Bmw class & call its method
+ModBMW = Bmw.Bmw()
+ModBMW.outModels()
+   
+# Create an object of Audi class & call its method
+ModAudi = Audi.Audi()
+ModAudi.outModels()
+  
+# Create an object of Nissan class & call its method
+ModNissan = Nissan.Nissan()
+ModNissan.outModels()
+
+
+
+"""
+from mypkg import print
+
+ma=print.abc()
+ma.printa('Aryamaan')
+
+"""
