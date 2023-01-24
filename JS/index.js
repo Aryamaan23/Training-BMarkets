bcb4c1dab3579ef69100bdc5cde3a0e1b43b1e0a
Aryamaan Pandey <aryamaanpandey@L-TMVR6D67G9.local>
2023-01-24 12:02:18 +0530

    Data types and calling external js

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+function checkAge(age)
+{
+    return age >18;
+}
+function myFunc()
+{
+    console.log(ages.find(checkAge));
+}
+const ages=[3,4,5,6,7,8,9]
+myFunc()
---
+++ JS/variables.js
@@ -0,0 +1,22 @@
+//numbers
+let l=4.5;
+let w=5.6;
+//Strings
+let s="Hello";
+let color="Pink";
+
+//Booleans
+let x=true;
+let y=false;
+console.log(x,y)
+//Object
+const emp={
+    "fName" : "Kailash",
+    "lName" : "Patil"
+}
+console.log(emp)
+//Array
+arr=["Aryamaan","Pandey","1A/2B","Bajaj Markets","SDE"]
+//Date 
+const d=new Date('2023-01-23')
+console.log(d)
