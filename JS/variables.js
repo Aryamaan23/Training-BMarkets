7bba47226c1faa9891af6ec18a0f8d307368c249
Aryamaan Pandey <aryamaanpandey@L-TMVR6D67G9.local>
2023-01-24 12:18:50 +0530

    Array functions

--- JS/variables.js
+++ JS/variables.js
@@ -17,6 +17,18 @@ const emp={
 console.log(emp)
 //Array
 arr=["Aryamaan","Pandey","1A/2B","Bajaj Markets","SDE"]
+const cars = new Array("Saab", "Volvo", "BMW");
 //Date 
 const d=new Date('2023-01-23')
 console.log(d)
+
+
+// Array methods
+console.log(cars.length)
+cars.sort()
+console.log(cars)
+
+//Accessing the array elements
+let c1=cars[0]
+console.log(c1)
+console.log(cars[cars.length-1])
