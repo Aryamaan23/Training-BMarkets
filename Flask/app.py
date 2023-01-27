1a21accb9b508b7c7b6d5b3ecaffcd9bf025008c
Aryamaan Pandey <aryamaanpandey@L-TMVR6D67G9.local>
2023-01-27 11:06:01 +0530

    Flask rendering the templates

--- Flask/app.py
+++ Flask/app.py
@@ -1,12 +1,20 @@
 #WSGI
 
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
-
+import datetime
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/educative")
+def learn():
+    return "Happy Learning at Educative!"
+
+@app.route("/temp")
+def temp():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
